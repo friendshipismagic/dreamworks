e713d09460156f52c6a5fbb6007c9feea384aea0
Clément Decoodt <clement@decoodt.eu>
2017-06-16 10:21:57 +0200

    WIP: Add default time-series viz

--- frontend/js/sidechart.js
+++ frontend/js/sidechart.js
@@ -22,9 +22,38 @@ $(document).ready(function() {
 		.y(function(d) { return y(d.close); });
 
 	// Adds the svg canvas
-	var svg = d3.select("sideChart")
+	var svg = d3.select("#sideChart")
 		.append("svg")
 		.attr("width", 100)
 		.attr("height", 100)
 		.append("g");
+
+	// Get the data
+	d3.csv("data.csv", function(error, data) {
+		data.forEach(function(d) {
+			d.date = parseDate(d.date);
+			d.close = +d.close;
+		});
+
+		// Scale the range of the data
+		x.domain(d3.extent(data, function(d) { return d.date; }));
+		y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+		// Add the valueline path.
+		svg.append("path")
+			.attr("class", "line")
+			.attr("d", valueline(data));
+
+		// Add the X Axis
+		svg.append("g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(0, 100)")
+			.call(xAxis);
+
+		// Add the Y Axis
+		svg.append("g")
+			.attr("class", "y axis")
+			.call(yAxis);
+
+	});
 });
