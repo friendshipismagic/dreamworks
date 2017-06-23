5ce32a338d06465be284f302e205ed7b04e05401
Tuetuopay <tuetuopay@me.com>
2017-06-23 11:02:02 +0200

    Extract most data out of the dreams

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -49,6 +49,50 @@ def scrape_dataset(name):
     dreams = []
     for tag in dreams_tags.find_all("input", {"type": "checkbox", "name": "d"}):
         L = [i for i in tag.next_siblings]
-        dreams.append(L[-2])
+
+        # There are several date formats in this f***ing dataset
+        m = (
+            re.search(r"\(([^/]+)/([^/]+)/([^/]+)\)", L[2]),    # MM/DD/YY
+            re.search(r"\(([^\-]+)-([^\-]+)-([^\-]+)\)", L[2]), # YY-MM-DD
+            re.search(r"\(\s*([0-9]+)\??\)", L[2]),             # YYYY
+            re.search(r"\((\w+) *(\d+)?, (\d+)\)", L[2]),       # Month DD, YYYY
+        )
+
+        months = [
+            "january", "february", "march", "april", "may", "june",
+            "july", "august", "september", "october", "november", "december"
+        ]
+        if m[0] is not None:
+            date = { "y": m[0].group(3), "m": m[0].group(1), "d": m[0].group(2) }
+        elif m[1] is not None:
+            date = { "y": m[1].group(1), "m": m[1].group(2), "d": m[1].group(3) }
+        elif m[2] is not None:
+            date = { "y": m[2].group(1), "m": "1", "d": "1" }
+        elif m[3] is not None:
+            date = {
+                "y": m[3].group(3),
+                "m": months.index(m[3].group(1).lower()) + 1,
+                "d": m[3].group(2)
+            }
+            if date["d"] is None: date["d"] = "01"
+        else:
+            print("Unrecognized date " + L[2])
+            date = { "y": "0000", "m": "01", "d": "01" }
+
+        print(L[2])
+
+        if date['y'] == "??": date['y'] = "01"
+        if date['m'] == "??": date['m'] = "01"
+        if date['d'] == "??": date['d'] = "01"
+        if len(date['y']) == 2: date['y'] = "19" + date['y']
+        date['y'] = int(date['y'])
+        date['m'] = int(date['m'])
+        date['d'] = int(date['d'])
+
+        dreams.append({
+            "text": L[-2],
+            "date": date
+        })
+        dreams.append((L[-2], L[2]))
     return dreams
 
